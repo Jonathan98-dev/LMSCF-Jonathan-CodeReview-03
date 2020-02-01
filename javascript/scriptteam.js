1f961d84822bf982e57c9c7fb8b9151bd10354e5
Jonathan Nuel <email.jonathannuel@gmail.com>
2020-02-01 01:18:53 +0100

    CodeReview 3 finished

--- javascript/scriptteam.js
+++ javascript/scriptteam.js
@@ -5,27 +5,31 @@ for(i=0; i < teamMembers.length; i++)
 {
 	document.getElementById("m_teamcontainer").innerHTML += ("<div class = \"members"+[i]+"\">" +teamMembers[i].name) ;
 	document.getElementById("m_teamcontainer").innerHTML += ("<div>" +teamMembers[i].email + "</div>");
-	document.getElementById("m_teamcontainer").innerHTML += ("<img id = \"img"+[i]+ "\" src = \""+ teamMembers[i].image + "\"></div><div id = \"tel"+[i]+"\"></div><br><br>");
+	document.getElementById("m_teamcontainer").innerHTML += ("<img id = \"img"+[i]+ "\" src = \""+ teamMembers[i].image + "\"></div><div id = \"tel"+[i]+"\"></div><div id = \"id"+[i]+"\"></div><br><br>");
 }
 
 
 function tel()
 {
 	document.getElementById("tel0").innerHTML = ("Tel: " + teamMembers[0].tel);
-	console.log("menas")
+	document.getElementById("id0").innerHTML = ("Id: " + teamMembers[0].Id);
+
+	console.log("test")
 }
 
 function tel1()
 {
 	document.getElementById("tel1").innerHTML = ("Tel: " + teamMembers[1].tel);
-	console.log("menas")
+	document.getElementById("id1").innerHTML = ("Id: " + teamMembers[0].Id);
+	console.log("test")
 }
 
 
 function tel2()
 {
 	document.getElementById("tel2").innerHTML = ("Tel: " + teamMembers[2].tel);
-	console.log("menas")
+	document.getElementById("id2").innerHTML = ("Id: " + teamMembers[0].Id);
+	console.log("test")
 }
 
 

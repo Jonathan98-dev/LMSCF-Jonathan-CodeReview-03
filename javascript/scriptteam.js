f43d1be0f47b3fb5cafd0c6017b5dd893ab7ba9d
Jonathan Nuel <email.jonathannuel@gmail.com>
2020-02-01 13:24:35 +0100

    done

--- javascript/scriptteam.js
+++ javascript/scriptteam.js
@@ -8,22 +8,21 @@ for(i=0; i < teamMembers.length; i++)
 	document.getElementById("m_teamcontainer").innerHTML += ("<img id = \"img"+[i]+ "\" src = \""+ teamMembers[i].image + "\"></div><div id = \"tel"+[i]+"\"></div><div id = \"id"+[i]+"\"></div><br><br>");
 }
 
-countertel1 = 1;
-countertel2 = 1;
-countertel3 = 1;
-
+countertel1 = true;
+countertel2 = true;
+countertel3 = true;
 
 
 function tel()
 {
 	
-	if(countertel1%2 != 0)
+	if(countertel1 == true)
 	{
 	document.getElementById("tel0").innerHTML = ("Tel: " + teamMembers[0].tel);
 	document.getElementById("id0").innerHTML = ("Id: " + teamMembers[0].Id);
 	console.log(countertel1)
 
-	countertel1++
+	countertel1=false;
 	}
 	else
 	{
@@ -31,7 +30,7 @@ function tel()
 	document.getElementById("id0").innerHTML = "";
 	console.log(countertel1)
 
-	countertel1++
+	countertel1=true
 	}
 
 	
@@ -39,13 +38,13 @@ function tel()
 
 function tel1()
 {
-	if(countertel2%2 != 0)
+	if(countertel2==true)
 	{
 	document.getElementById("tel1").innerHTML = ("Tel: " + teamMembers[1].tel);
 	document.getElementById("id1").innerHTML = ("Id: " + teamMembers[1].Id);
 	console.log(countertel2)
 
-	countertel2++
+	countertel2=false
 	}
 	else
 	{
@@ -53,20 +52,20 @@ function tel1()
 	document.getElementById("id1").innerHTML = "";
 	console.log(countertel2)
 
-	countertel2++
+	countertel2=true
 	}
 }
 
 
 function tel2()
 {
-	if(countertel3%2 != 0)
+	if(countertel3==true)
 	{
 	document.getElementById("tel2").innerHTML = ("Tel: " + teamMembers[2].tel);
 	document.getElementById("id2").innerHTML = ("Id: " + teamMembers[2].Id);
 	console.log(countertel3)
 
-	countertel3++
+	countertel3=false
 	}
 	else
 	{
@@ -74,7 +73,7 @@ function tel2()
 	document.getElementById("id2").innerHTML = "";
 	console.log(countertel3)
 
-	countertel3++
+	countertel3=true
 	}
 }
 
